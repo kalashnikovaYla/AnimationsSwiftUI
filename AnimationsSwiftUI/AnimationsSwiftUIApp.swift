3cf59391509e59735db7ab17656f49dcc506f852
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-11 23:07:49 +0500

    Initial Commit

---
+++ AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by sss on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
