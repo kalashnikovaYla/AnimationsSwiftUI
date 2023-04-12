4bb2864b2591a3f5e9b53dff71b6dbfff8547703
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-12 09:32:42 +0500

    Initial

--- AnimationsSwiftUI/ContentView.swift
+++ AnimationsSwiftUI/ContentView.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isStated = false
+    @State var circleProgress: CGFloat = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            ZStack {
+                
+                //1
+                Button {} label: {
+                    Image(systemName: "bag.badge.plus")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 15, height: 15)
+                        .padding()
+                        .clipShape(Circle())
+                        .rotationEffect(Angle(degrees: isStated ? 0: -90))
+                }
+                .background(Circle().fill(.orange).shadow(radius: 8, x: 4, y: 4))
+                .offset(x: 0, y: isStated ? -70: 0)
+                .opacity(isStated ? 1: 0)
+                
+                //2
+                Button {} label: {
+                    Image(systemName: "calendar.badge.plus")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 15, height: 15)
+                        .padding()
+                        .clipShape(Circle())
+                        .rotationEffect(Angle(degrees: isStated ? 0: -90))
+                }
+                .background(Circle().fill(.orange).shadow(radius: 8, x: 4, y: 4))
+                .offset(x: isStated ? -70: 0, y: 0)
+                .opacity(isStated ? 1: 0)
+                
+                
+                //3
+                
+                Button {
+                    withAnimation(.spring()) {
+                        isStated.toggle()
+                    }
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 15, height: 15)
+                        .padding()
+                        .clipShape(Circle())
+                        .rotationEffect(Angle(degrees: isStated ? 0: -90))
+                }
+                .background(Circle().fill(.orange).shadow(radius: 8, x: 4, y: 4))
+                
+            }
+            Spacer()
         }
         .padding()
     }
